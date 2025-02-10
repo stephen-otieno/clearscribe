f50958cb72060cc9c53fca6c4e60e9b9417d37c6
stephen-otieno <stephenotieno8781@gmail.com>
2025-02-10 15:20:46 +0300

    staticfiles

--- ClearScribe/urls.py
+++ ClearScribe/urls.py
@@ -16,10 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+from ClearScribe import settings
 from transcriptions import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='homepage'),
     path('contact Us/', views.contact_us, name='contact_us'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
